349fd1737464d541a6106e4f442ae5488d1e68b1
Brian Hurst <brian.hurst@gsa.gov>
2023-05-24 14:33:16 -0400

    Add ruby

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,9 +3,9 @@ module.exports = {
       collect: {
         startServerCommand: 'bundle exec jekyll serve',
         url: ['http://localhost:4000']
-    },
+      },
       upload: {
-        target: 'temporary-public-storage',
-    },
+        target: 'temporary-public-storage'
+      },
     },
   };
