95d6a6b1a4e4f2f881fe5b815150d25b7e13984b
Brian Hurst <brian.hurst@gsa.gov>
2023-06-02 11:30:20 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,9 @@ module.exports = {
       //   preset: "lighthouse:recommended",
       // },
       collect: {
+        settings: {
+          plugins: ["lighthouse-plugin-edx"],
+        },
         startServerCommand: 'bundle exec jekyll serve',
         url: ['http://localhost:4000']
       },
